5b317655b751112b570ed9c9c40e18947d7189e8
Athena <athenasu210@gmail.com>
2021-11-29 11:18:01 +0800

    member dao changed

--- src/main/java/tw/idv/tibame/tfa104/shanshan/web/member/dao/impl/MemberDaoImpl.java
+++ src/main/java/tw/idv/tibame/tfa104/shanshan/web/member/dao/impl/MemberDaoImpl.java
@@ -16,11 +16,11 @@ import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import tw.idv.tibame.tfa104.shanshan.web.member.dao.Memberhello;
+import tw.idv.tibame.tfa104.shanshan.web.member.dao.MemberDao;
 import tw.idv.tibame.tfa104.shanshan.web.member.entity.Member;
 
 @Repository
-public class MemberDaoImpl implements Memberhello {
+public class MemberDaoImpl implements MemberDao {
 
 	@Autowired
 	private SessionFactory sessionFactory;
